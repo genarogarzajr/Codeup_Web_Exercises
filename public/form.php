c31529f843eacbc2e054ac3cf0ed4b775d997623
Genaro Garza <genarogarza@MyMacbook.local>
2014-05-23 10:48:21 -0500

    Text, Password, and Text Area exercises completed

--- public/form.php
+++ public/form.php
@@ -22,7 +22,7 @@
         print_r($_POST);
     ?>
 
-            <form method="POST" >
+<!--             <form method="POST" >
                 <p>
                     <label for="username">Username</label>
                     <input id="username" name="user" type="text" placeholder="username">
@@ -31,11 +31,39 @@
                     <label for="password">Password</label>
                     <input id="password" name="pass" type="password" placeholder="password">
                 </p>
+                
                 <p>
-                    <!-- <input type="submit" value="process"> -->
-
                     <button type="submit">Login</button>
                 </p>
+            </form> -->
+
+<hr>
+
+            <form method="POST">
+                <p>
+                    <label for="to">To:</label>
+                    <input id="to" name="to" type="email" placeholder="to">
+                </p>
+                <p>
+                    <label for="from">From:</label>
+                    <input id="from" name="from" type="text" placeholder="email">
+                </p>
+                <p>
+                    <label for="subject">Subject:</label>
+                    <input id="subject" name="subject" type="text" placeholder="subject">
+                </p>
+                <p>
+                    <textarea name="body" rows="5" cols="50" placeholder="type message here"></textarea>
+
+
+                <p>
+                    <button type="submit">Submit</button>
+                </p>
+
+
+
+
+
             </form>
 
 
