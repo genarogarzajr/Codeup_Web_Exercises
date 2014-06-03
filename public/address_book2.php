7e64ecd8d1f233f78c69488e8ca7d77c8cffa373
Genaro Garza <github.rf9@gishpuppy.com>
2014-06-03 10:53:22 -0500

    reading csv files completed

--- public/address_book2.php
+++ public/address_book2.php
@@ -7,30 +7,57 @@ $address_book = [
     ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
 ];
 
+
+
 $filename = "data/adr_bk.csv";
 
+
+
+
+
+
+//---------------------------------------------
 function write_csv($x_bigArray, $x_filename)
 {
-	if(is_writable($x_filename))
-		{
-			$x_handle = fopen($x_filename, "W");
+	//if(is_writable($x_filename))
+		//{
+			$x_handle = fopen($x_filename, "w");
 			foreach ($x_bigArray as $x_fields) 
 			{
 				fputcsv($x_handle, $x_fields);
 			}
 			fclose($x_handle);
-		}	
+		//}	
 }
+//---------------------------------------------
 
-if (!empty($_POST)) 
-	{
-		$new_add = [];
-		foreach ($_POST as $key => $value) 
-		{
-			$new_add[] = $value;
-		}
-	}
 
+
+
+
+//>>>>>>>>CODE STARTS HERE<<<<<<<<<<<<<<<<<<<<
+$new_address = [];
+// Error check
+if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['zip'])) 
+{
+    $new_address['name'] = $_POST['name'];
+    $new_address['address'] = $_POST['address'];
+    $new_address['city'] = $_POST['city'];
+    $new_address['state'] = $_POST['state'];
+    $new_address['zip'] = $_POST['zip'];
+    $new_address['phone'] = $_POST['phone'];
+    $address_book[] = $new_address;
+
+	write_csv($address_book, $filename);
+    
+} else {
+
+    foreach ($_POST as $key => $value) {
+        if (empty($value)) {
+            echo "<h1>" . ucfirst($key) .  " is empty.</h1>";
+        }
+    }
+}
 
 
 ?>
@@ -44,13 +71,14 @@ if (!empty($_POST))
 <h2>Writing CSV file Exercise 2</h2>
 
 
-<table>
+<table border = 1>
 <tr>
 	<th>Name</th>
 	<th>Address</th>
 	<th>City</th>
 	<th>State</th>
 	<th>Zip</th>
+	<th>Phone</th>
 </tr>	
 <? foreach ($address_book as $fields) : ?>
 <tr>
@@ -63,21 +91,23 @@ if (!empty($_POST))
 
 <form method="POST">
     <p>
-        <label for="Name">Name</label>
-        <input id="Name" name="Name" type="text">
+        <label for="name">Name</label>
+        <input id="name" name="name" type="text">
         <br>
-        <label for="Address">Address</label>
-        <input id="Address" name="Address" type="text">
+        <label for="address">Address</label>
+        <input id="address" name="address" type="text">
         <br>
-        <label for="City">City</label>
-        <input id="City" name="City" type="text">
+        <label for="city">City</label>
+        <input id="city" name="city" type="text">
         <br>
-        <label for="State">State</label>
-        <input id="State" name="State" type="text">
+        <label for="state">State</label>
+        <input id="state" name="state" type="text">
         <br>
-        <label for="Zip">Zip</label>
-        <input id="Zip" name="Zip" type="text">
+        <label for="zip">Zip</label>
+        <input id="zip" name="zip" type="text">
         <br>
+        <label for="phone">Phone</label>
+        <input id="phone" name="phone" type="text">
     </p>
     
     <p>
